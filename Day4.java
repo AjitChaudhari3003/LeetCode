a04c75f5a4eb8e9a035995bfbd23cd269a6b490f
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2024-12-29 17:34:00 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -53,3 +53,36 @@ class Solution {
       return maxLength;  
     }
 }
+
+//end
+
+
+
+//  Minimum Size Subarray Sum
+
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray
+// Example 1:
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+// CODE
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLength = Integer.MAX_VALUE;
+        int sum = 0, start = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            while (sum >= target) {
+                minLength = Math.min(minLength, i- start + 1);
+                sum -= nums[start++];
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
+
