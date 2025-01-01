9362f41a9da76268eec34b3baf14c401710db573
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-01-01 23:37:37 +0530

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,51 @@
+// Next Permutation
+
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+// Example 2:
+// Input: nums = [3,2,1]
+// Output: [1,2,3]
+// Example 3:
+// Input: nums = [1,1,5]
+// Output: [1,5,1]
+
+// CODE
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+
+        int i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+         if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+
+}
