ee5cf38f1d06eca80d72c951105fd21bcd97e803
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-01-09 23:08:58 +0530

    Update Day15.java

--- Day15.java
+++ Day15.java
@@ -17,3 +17,27 @@ class Solution {
         return nums.length;
     }
 }
+
+// end
+
+// 88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+// CODE
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        for( int i=0;i<n;i++){
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+       
+    }
+}
