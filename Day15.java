9c2a16f751fcc5d0f32aa30299d1dc3480e048d7
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-01-09 22:26:46 +0530

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,19 @@
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// CODE
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(target<=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
