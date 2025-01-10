449d6f412a7053dfae486342af052a49641878ef
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-01-10 20:21:18 +0530

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -16,3 +16,30 @@ class Solution {
          return res;
     }
 }
+
+// end
+
+// 169. Majority Elements
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// CODE
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    
+    }
+}
