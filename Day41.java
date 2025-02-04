015bdba0a9d0b64c5f54587d15d2e77420e75ac1
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-02-04 21:32:32 +0530

    Create Day41.java

---
+++ Day41.java
@@ -0,0 +1,21 @@
+// 28. Find the Index of the First Occurrence in a String
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// CODE
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
