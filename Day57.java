1596f03b7be7da4d459da0591284f02de0e1826b
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-02-20 21:23:48 +0530

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,14 @@
+// 709. To Lower Case
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+
+// CODE
+
+class Solution {
+    public String toLowerCase(String s) {
+       return s.toLowerCase();
+    }
+}
