f4d9d203745128fd7e2c054210a5cea587a6934c
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-03-03 19:43:34 +0530

    Update Day68.java

--- Day68.java
+++ Day68.java
@@ -20,19 +20,24 @@
 
 //   CODE
 
-class Solution {
+public class Solution {
     public boolean checkRecord(String s) {
-        int A=0;
-        for(int i=0;i< s.length();i++){
-            if(s.charAt(i)=='A'){
-                A++;
-                if(A>=2)
-                return false;
+        int lateCount = 0;
+        int absentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'A') {
+                absentCount++;
+                if (absentCount >= 2) return false;
             }
-        
-        if(i >=2 &&s.charAt(i) == 'L' && s.charAt(i - 1) == 'L' && s.charAt(i - 2) == 'L')
-        return false;
+            if (c == 'L') {
+                lateCount++;
+                if (lateCount == 3) return false;
+            } else {
+                lateCount = 0;
+            }
         }
-        return true;
+        return absentCount < 2 && lateCount < 3;
     }
 }
