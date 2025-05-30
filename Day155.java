cf649b9e2e51acc99c9cfc000d84b0c4c1635a96
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-05-30 11:24:47 +0530

    Create Day155.java

---
+++ Day155.java
@@ -0,0 +1,59 @@
+// 2359. Find Closest Node to Given Two Nodes
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
+
+// The graph is represented with a given 0-indexed array edges of size n, indicating that there is a directed edge from node i to node edges[i]. If there is no outgoing edge from i, then edges[i] == -1.
+
+// You are also given two integers node1 and node2.
+
+// Return the index of the node that can be reached from both node1 and node2, such that the maximum between the distance from node1 to that node, and from node2 to that node is minimized. If there are multiple answers, return the node with the smallest index, and if no possible answer exists, return -1.
+
+// Note that edges may contain cycles.
+
+ 
+
+// Example 1:
+
+
+// Input: edges = [2,2,3,-1], node1 = 0, node2 = 1
+// Output: 2
+// Explanation: The distance from node 0 to node 2 is 1, and the distance from node 1 to node 2 is 1.
+// The maximum of those two distances is 1. It can be proven that we cannot get a node with a smaller maximum distance than 1, so we return node 2.
+
+
+class Solution {
+    public int[] dfs(int[] edges, int start) {
+        int[] dist = new int[edges.length];
+        Arrays.fill(dist, -1);
+        int d = 0;
+        while (start != -1 && dist[start] == -1) {
+            dist[start] = d++;
+            start = edges[start];
+        }
+        return dist;
+    }
+
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int[] dist1 = dfs(edges, node1);
+        int[] dist2 = dfs(edges, node2);
+        int result = -1;
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < edges.length; i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int maxDist = Math.max(dist1[i], dist2[i]);
+                if (maxDist < minDist) {
+                    minDist = maxDist;
+                    result = i;
+                }
+            }
+        }
+
+        return result;
+    }
+}
