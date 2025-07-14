dffc90d846009d429d65b871fcd0202012c278ef
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-07-14 10:51:06 +0530

    Create Day200.java

---
+++ Day200.java
@@ -0,0 +1,45 @@
+// 1290. Convert Binary Number in a Linked List to Integer
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,0,1]
+// Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int count=1;
+        int num=0;
+        ListNode cur=head;
+
+        while(cur !=null){
+            cur = cur.next;
+            count *=2;
+        }
+
+        count /=2;
+        cur=head;
+
+        while(cur !=null){
+            num +=cur.val*count;
+            count /=2;
+            cur = cur.next;
+        }
+        return num;
+    }
+}
