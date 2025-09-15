17c9156a8eeb517730199e162cec246280bc35c5
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-09-15 08:20:41 +0530

    Create Day263.java

---
+++ Day263.java
@@ -0,0 +1,36 @@
+// 1935. Maximum Number of Words You Can Type
+// There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+// Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+// Example 1:
+// Input: text = "hello world", brokenLetters = "ad"
+// Output: 1
+// Explanation: We cannot type "world" because the 'd' key is broken.
+// Example 2:
+// Input: text = "leet code", brokenLetters = "lt"
+// Output: 1
+// Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+// Example 3:
+// Input: text = "leet code", brokenLetters = "e"
+// Output: 0
+// Explanation: We cannot type either word because the 'e' key is broken.
+
+
+
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int res = 0;
+        int wend = 0;
+
+        while (wend < text.length()) {
+            if (brokenLetters.indexOf(text.charAt(wend)) != -1) {
+                while (wend < text.length() && text.charAt(wend) != ' ') {
+                    wend += 1;
+                }
+            } else if (text.charAt(wend) == ' ' || wend == text.length() - 1) {
+                res += 1;
+            }
+            wend += 1;
+        }
+        return res;
+    }
+}
