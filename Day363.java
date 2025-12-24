0642473e29a7dfe1943bf923fad23281f8097f8c
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2025-12-24 12:37:22 +0530

    Create Day363.java

---
+++ Day363.java
@@ -0,0 +1,44 @@
+// 3074. Apple Redistribution into Boxes
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an array apple of size n and an array capacity of size m.
+
+// There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+// Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+// Note that, apples from the same pack can be distributed into different boxes.
+
+ 
+
+// Example 1:
+
+// Input: apple = [1,3,2], capacity = [4,3,1,5,2]
+// Output: 2
+// Explanation: We will use boxes with capacities 4 and 5.
+// It is possible to distribute the apples as the total capacity is greater than or equal to the total number of apples
+
+
+
+
+
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int[] hash = new int[51];
+        int ans = 0, sum = 0;
+        for(int i = 0; i < apple.length; i++) sum += apple[i];
+        for(int i = 0; i < capacity.length; i++) hash[capacity[i]]++;
+        for(int i = 50; i > 0; i--) {
+            while(hash[i] > 0) {
+                sum -= i;
+                hash[i]--;
+                ans++;
+                if(sum <= 0) return ans;
+            }
+        }
+        return ans;
+    }
+}
